a8c8dfe373a04f85933282d25a693fb26fca2d2c
doesthecenterhold <mr.dobrevski@gmail.com>
2024-04-15 15:55:06 +0200

    still working on it

---
+++ launch/sim_turtlebot_nav.launch.py
@@ -0,0 +1,113 @@
+# Copyright 2023 Clearpath Robotics, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# @author Roni Kreinin (rkreinin@clearpathrobotics.com)
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+
+pkg_dis_tutorial3 = get_package_share_directory('dis_tutorial3')
+
+
+ARGUMENTS = [
+    DeclareLaunchArgument('namespace', default_value='',
+                          description='Robot namespace'),
+    DeclareLaunchArgument('rviz', default_value='true',
+                          choices=['true', 'false'], description='Start rviz.'),
+    DeclareLaunchArgument('world', default_value='dis',
+                          description='Ignition World'),
+    DeclareLaunchArgument('model', default_value='standard',
+                          choices=['standard', 'lite'],
+                          description='Turtlebot4 Model'),
+    DeclareLaunchArgument('map', default_value=PathJoinSubstitution(
+                          [pkg_dis_tutorial3, 'maps', 'map.yaml']),
+                          description='Full path to map yaml file to load'),
+]
+
+for pose_element in ['x', 'y', 'z', 'yaw']:
+    ARGUMENTS.append(DeclareLaunchArgument(pose_element, default_value='0.0',
+                     description=f'{pose_element} component of the robot pose.'))
+
+
+def generate_launch_description():
+    # Directories
+    package_dir_robot = get_package_share_directory('dis_tutorial7')
+
+    # Paths
+    ignition_launch = PathJoinSubstitution(
+        [pkg_dis_tutorial3, 'launch', 'ignition.launch.py'])
+    robot_spawn_launch = PathJoinSubstitution(
+        [package_dir_robot, 'launch', 'turtlebot4_spawn.launch.py'])
+    localization_launch = PathJoinSubstitution(
+        [pkg_dis_tutorial3, 'launch', 'localization.launch.py'])
+    nav2_launch = PathJoinSubstitution(
+        [pkg_dis_tutorial3, 'launch', 'nav2.launch.py'])
+
+    # Launch configurations
+    namespace = LaunchConfiguration('namespace')
+    map_file = LaunchConfiguration('map')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    x, y, z = LaunchConfiguration('x'), LaunchConfiguration('y'), LaunchConfiguration('z')
+    yaw = LaunchConfiguration('yaw')
+
+    ignition = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([ignition_launch]),
+        launch_arguments=[
+            ('world', LaunchConfiguration('world'))
+        ]
+    )
+
+    robot_spawn = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([robot_spawn_launch]),
+        launch_arguments=[
+            ('namespace', LaunchConfiguration('namespace')),
+            ('rviz', LaunchConfiguration('rviz')),
+            ('x', LaunchConfiguration('x')),
+            ('y', LaunchConfiguration('y')),
+            ('z', LaunchConfiguration('z')),
+            ('yaw', LaunchConfiguration('yaw'))]
+    )
+
+    # Localization
+    localization = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([localization_launch]),
+        launch_arguments=[
+            ('namespace', namespace),
+            ('use_sim_time', use_sim_time),
+            ('map', map_file),
+        ]
+    )
+
+    # Nav2
+    nav2 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([nav2_launch]),
+        launch_arguments=[
+            ('namespace', namespace),
+            ('use_sim_time', use_sim_time)
+        ]
+    )
+
+    # Create launch description and add actions
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(ignition)
+    ld.add_action(robot_spawn)
+    ld.add_action(localization)
+    ld.add_action(nav2)
+    return ld
